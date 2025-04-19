4c08961ada33236519c181d68916751a2fc26bfd
Edward Tanguay <edward@tanguay.info>
2025-04-19 14:59:19 +0200

    base works again

---
+++ scripts/CreatePage.hs
@@ -0,0 +1,10 @@
+module CreatePage where
+
+import Math (add, multiply)
+
+main :: IO ()
+main = do
+    let sumResult = add 5 4
+    let productResult = multiply 5 3
+    putStrLn ("Sum: " ++ show sumResult)
+    putStrLn ("Product: " ++ show productResult)
---
+++ scripts/Math.hs
@@ -0,0 +1,7 @@
+module Math (add, multiply) where
+
+add :: Int -> Int -> Int
+add x y = x + y
+
+multiply :: Int -> Int -> Int
+multiply x y = x * y
